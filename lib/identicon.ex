960f25c74e0c3603a7aeedfd85ad126784f72499
Kevin Asbury <kasbury@pursuitky.com>
2019-02-25 21:42:24 -0500

    MD5 hash input string

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,5 +1,15 @@
 defmodule Identicon do
-  def hello do
-    :world
+  def main(input) do
+    input
+    |> hash_input
+    |> list_of_numbers
+  end
+
+  def hash_input(input) do
+    :crypto.hash(:md5, input)
+  end
+
+  def list_of_numbers(input) do
+    :binary.bin_to_list(input)
   end
 end
